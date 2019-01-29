b5a348cfee7bad9dff234e522f98975db6e3c45e
Marc Cometa <menfcometa@gmail.com>
2019-01-29 14:54:05 +0800

    Update Avatar.vue
    Remove inline font-family

--- src/Avatar.vue
+++ src/Avatar.vue
@@ -45,7 +45,6 @@ export default {
     textStyle() {
       return {
         fill: 'rgba(255, 255, 255, .7)',
-        fontFamily: "'Lucida Console', Monaco, monospace",
         fontSize: `${this.size * 0.65}px`
       };
     }
